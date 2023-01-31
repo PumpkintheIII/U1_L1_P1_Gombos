ead717303ecdfb586f13102a18551cb2b20ac595
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-01-31 14:24:23 +0000

    added code to ask for numbers and get output their sum

--- .ccls-cache/@home@runner@U1L1P1Gombos/main.c
+++ .ccls-cache/@home@runner@U1L1P1Gombos/main.c
@@ -6,5 +6,20 @@
 #include <stdio.h>
 
 int main(void) {
+  int num1, num2, num3, numSum;
   
+  printf("Please enter a number: ");
+  scanf("%d", &num1);
+  getchar();
+  printf("Please enter a number: ");
+  scanf("%d", &num2);
+  getchar();
+  printf("Please enter a number: ");
+  scanf("%d", &num3);
+  getchar();
+
+  numSum = num1 + num2;
+  numSum += num3;
+  printf("Sum: %d", numSum);
+  return 0;
 }
