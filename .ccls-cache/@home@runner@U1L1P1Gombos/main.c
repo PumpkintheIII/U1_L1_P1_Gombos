5077db1a236cbea91cce91984df18ef9f40c56b5
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-01 13:42:39 +0000

    allows you to use decimal points

--- .ccls-cache/@home@runner@U1L1P1Gombos/main.c
+++ .ccls-cache/@home@runner@U1L1P1Gombos/main.c
@@ -1,25 +1,26 @@
 /*
   UB C Programming Unit 1 Lesson 2 Program 1
+  Sum of Entered Numbers
   Max Gombos
   */
 
 #include <stdio.h>
 
 int main(void) {
-  int num1, num2, num3, numSum;
+  float num1, num2, num3, numSum;
   
   printf("Please enter a number: ");
-  scanf("%d", &num1);
+  scanf("%f", &num1);
   getchar();
   printf("Please enter a number: ");
-  scanf("%d", &num2);
+  scanf("%f", &num2);
   getchar();
   printf("Please enter a number: ");
-  scanf("%d", &num3);
+  scanf("%f", &num3);
   getchar();
 
   numSum = num1 + num2;
   numSum += num3;
-  printf("Sum: %d", numSum);
+  printf("Sum: %f", numSum);
   return 0;
 }
