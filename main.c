def5f7eb347b8d7a5e0be2fa09bb040313636af6
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-01 13:48:02 +0000

    added comments

--- main.c
+++ main.c
@@ -4,23 +4,34 @@
   Max Gombos
   */
 
-#include <stdio.h>
+#include <stdio.h> //imports functions
 
-int main(void) {
+int main(void) { //defines main function
   float num1, num2, num3, numSum;
-  
-  printf("Please enter a number: ");
-  scanf("%f", &num1);
-  getchar();
-  printf("Please enter a number: ");
-  scanf("%f", &num2);
-  getchar();
-  printf("Please enter a number: ");
-  scanf("%f", &num3);
-  getchar();
+  //Initialize variables used as float variables
 
-  numSum = num1 + num2;
-  numSum += num3;
-  printf("Sum: %f", numSum);
-  return 0;
+  printf("Please enter a number: ");
+  //asks user for the first number
+  scanf("%f", &num1);
+  //accepts the users input and formats it as a float
+  getchar();
+  //asks user for number
+  printf("Please enter a number: ");
+  //asks user for the second number
+  scanf("%f", &num2);
+  //accepts the users input and formats it as a float
+  getchar();
+  //asks user for number
+  printf("Please enter a number: ");
+  //asks user for the third number
+  scanf("%f", &num3);
+  //accepts the users input and formats it as a float
+  getchar();
+  //asks user for number
+
+  //Code to find the sum of numbers:
+  numSum = num1 + num2; //adds num1 and num2
+  numSum += num3; //adds num3 to num1 and num2
+  printf("Sum: %f", numSum); //prints the output
+  return 0; //ends the program
 }
